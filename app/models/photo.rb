622cfe7f83561169546a818e96cde39837ad703d
Raghu Betina <raghu@firstdraft.com>
2020-08-17 15:02:27 +0000

    Indirect association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,6 +16,10 @@ class Photo < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :followers,
+             :through => :owner,
+             :source => :followers
+
   # Validations
 
   validates :image, :presence => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe User, type: :model do
 
     describe "InDirect Associations" do
 
+    it { should have_many(:timeline) }
+
     end
 
     describe "Validations" do
